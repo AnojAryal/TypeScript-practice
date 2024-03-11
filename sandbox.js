160370281f039f809456765f089df2bcc9295678
anojaryal <anoj1810@gmail.com>
2024-03-11 22:09:30 +0545

    type basics

--- sandbox.js
+++ sandbox.js
@@ -5,3 +5,15 @@ console.log(input1);
 input1.forEach(function (input) {
     console.log(input);
 });
+var age = 21;
+var isGirl = false;
+//we can change the value of a variable of same type.
+age = 22;
+//cant change the value to different type
+//age = 'twenty two 22'
+//circumference of a circle
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(12));
+//ts allow us to typecheck during development 
--- sandbox.ts
+++ sandbox.ts
@@ -8,3 +8,22 @@ console.log(input1);
 input1.forEach(input => {
     console.log(input)
 })
+
+
+let age = 21;
+let isGirl = false;
+
+//we can change the value of a variable of same type.
+age = 22;
+
+//cant change the value to different type
+//age = 'twenty two 22'
+
+
+//circumference of a circle
+const circ = (diameter : number) => {  // in ts we can define the type of a variable 
+    return diameter * Math.PI;
+}
+
+console.log(circ(12));
+//ts allow us to typecheck during development 

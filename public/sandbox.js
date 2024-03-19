d31291f8e05055288f9683a8a49736bbd6911bbc
anojaryal <anoj1810@gmail.com>
2024-03-19 21:50:47 +0545

    function signature in ts

--- public/sandbox.js
+++ public/sandbox.js
@@ -101,3 +101,18 @@ let meet;
 meet = (name, greeting) => {
     console.log(`${name} says ${greeting}`);
 };
+//example 2
+let calc;
+calc = (numOne, numTwo, action) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+//example 3
+let logDetails1;
+logDetails1 = (ninja) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -178,3 +178,24 @@ let meet : (x : string, y: string)=> void;
 meet = (name : string, greeting : string) => {
     console.log(`${name} says ${greeting}`);  
 }
+
+//example 2
+let calc : (x: number, y: number, z: string) => number;
+
+calc = (numOne: number, numTwo: number, action : string) => {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }else {
+        return numOne - numTwo;
+    }
+}
+
+//example 3
+let logDetails1 : (obj :{name : string, age : number}) => void;
+
+
+type person = {name: string , age : number};
+
+logDetails1 = (ninja: person) => {
+    console.log(`${ninja.name} is ${ninja.age} years old`);
+}

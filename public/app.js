3c8a3077ad239e04b18636b8e5fe70779ee6c477
anojaryal <anoj1810@gmail.com>
2024-03-26 21:47:16 +0545

    INterfaces with classes

--- public/app.js
+++ public/app.js
@@ -1,29 +1,22 @@
-const me = {
-    name: 'Anoj',
-    age: 21,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spend', amount);
-        return amount;
-    }
-};
-const greetPerson = (person) => {
-    console.log('hello', person.name);
-};
-greetPerson(me);
-console.log(me);
 import { Invoice } from './classes/invoice.js';
-// const form = document.querySelector('form')!;
-const invOne = new Invoice('FraNzY', 'WOrk on FraNzY website', 350);
-const invTwo = new Invoice('Asmi', 'WOrk on Asmi website', 250);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Payment } from './classes/payment.js';
+// let docOne : HasFormatter;
+// let docTwo : HasFormatter;
+// docOne = new Invoice('FraNzY', 'Web Work', 250);
+// docTwo = new Payment('Asmi', 'Plumbing Work', 200);
+// let docs : HasFormatter[] =[];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// // const form = document.querySelector('form')!;
+// const invOne = new Invoice('FraNzY', 'WOrk on FraNzY website', 350);
+// const invTwo = new Invoice('Asmi', 'WOrk on Asmi website', 250);
+// let invoices : Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach(inv => {
+//     console.log(inv.client, inv.amount, inv.format());
+// })
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
 //inputs 
@@ -33,5 +26,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,34 @@
 import {Invoice} from './classes/invoice.js';
+import {Payment} from './classes/payment.js';
+import { HasFormatter } from './interfaces/hasFormatter.js';
 
-// const form = document.querySelector('form')!;
-const invOne = new Invoice('FraNzY', 'WOrk on FraNzY website', 350);
-const invTwo = new Invoice('Asmi', 'WOrk on Asmi website', 250);
 
-let invoices : Invoice[] = [];
+// let docOne : HasFormatter;
+// let docTwo : HasFormatter;
 
-invoices.push(invOne);
-invoices.push(invTwo);
+// docOne = new Invoice('FraNzY', 'Web Work', 250);
+// docTwo = new Payment('Asmi', 'Plumbing Work', 200);
 
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-})
+// let docs : HasFormatter[] =[];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+// console.log(docs);
+
+
+// // const form = document.querySelector('form')!;
+// const invOne = new Invoice('FraNzY', 'WOrk on FraNzY website', 350);
+// const invTwo = new Invoice('Asmi', 'WOrk on Asmi website', 250);
+
+// let invoices : Invoice[] = [];
+
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach(inv => {
+//     console.log(inv.client, inv.amount, inv.format());
+// })
+
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -27,11 +44,13 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit',(e : Event)=> {
     e.preventDefault();
 
-    console.log(
-        type.value,
-        tofrom.value,
-        details.value,
-        amount.valueAsNumber,
-    );
+    let doc : HasFormatter;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    }else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+
+    console.log(doc);
     
 })

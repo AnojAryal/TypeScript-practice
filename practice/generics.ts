7f4c4ea23e1c82988d44ff099a5a86ea61ba2a0f
anojaryal <anoj1810@gmail.com>
2024-03-31 21:48:20 +0545

    generics with interface

--- practice/generics.ts
+++ practice/generics.ts
@@ -1,9 +1,26 @@
-const addUID = (obj : object) => {
+const addUID = <T extends object>(obj : T) => {
     let uid = Math.floor(Math.random() * 100);
     return {...obj, uid};
 }
 
 let docOne = addUID({name : 'yoshi', age : 40});
+//let docTwo = addUID('FraNzY')
+
+console.log(docOne.name);
 
 
-console.log(docOne);
+//with interfaces
+
+interface Resource<T> {
+    uid : number;
+    resourceName : string;
+    data : T;
+};
+
+const docThree: Resource<object> = {
+    uid : 1,
+    resourceName : 'person',
+    data : {name : 'Ramu'}
+}
+
+console.log(docThree);

f8a114f5f1b1d062028cf9b60b9e9bdec0b58701
anojaryal <anoj1810@gmail.com>
2024-04-03 14:29:01 +0545

    tuples in ts

--- practice/tuples.ts
+++ practice/tuples.ts
@@ -2,3 +2,12 @@ let arr = ['Sus', 18, true];
 arr[0] = false
 arr[1] = 'Ramu'
 arr = [45,false,'Gopal'];
+
+let tup: [string, number, boolean]=['Anoj', 25, true];
+tup[0]= 'Shyamu';
+tup[1] = 18;
+tup[2]= false;
+
+
+let student : [string, number];
+student = ['Bishnu', 71];
